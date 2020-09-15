33dddc6a4d7401f4619d00593de107cc669801f2
lakesxo <olaksajanaku@yahoo.com>
2020-09-15 23:31:37 +0100

    added mobile nav

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,9 @@
 let menu = document.querySelector('.hamburger');
+let navs = document.querySelector('.mobile-nav')
 
 menuToggle =()=> {
     menu.classList.toggle('change')
+    navs.classList.toggle('hide')
 }
 
 menu.addEventListener('click', menuToggle)
