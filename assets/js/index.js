42db2a8fee2a6df8c73be97bbd1a39c99f03554f
lakesxo <olaksajanaku@yahoo.com>
2020-10-05 16:55:19 +0100

    updated experience

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,32 @@
 let menu = document.querySelector('.hamburger');
-let navs = document.querySelector('.mobile-nav')
+let navs = document.querySelector('.mobile-nav');
+let hng = document.querySelector('.hng');
+let migrant = document.querySelector('.migrant');
+let more = document.querySelector('.more');
+let hngBtn = document.querySelector('li.top');
+let moreBtn = document.querySelector('li.mid');
+let migrantBtn = document.querySelector('li.last');
 
 menuToggle =()=> {
-    menu.classList.toggle('change')
-    navs.classList.toggle('hide')
+    menu.classList.toggle('change');
+    navs.classList.toggle('hide');
 }
-
-menu.addEventListener('click', menuToggle)
+hngFunc =()=> {
+    hng.style.display = "block";
+    more.style.display = "none";
+    migrant.style.display = "none"
+}
+migrantFunc =()=> {
+    migrant.style.display = "block";
+    more.style.display = "none";
+    hng.style.display = "none";
+}
+moreFunc =()=> {
+    more.style.display = "block";
+    migrant.style.display = "none"
+    hng.style.display = "none"
+}
+menu.addEventListener('click', menuToggle);
+hngBtn.addEventListener('click', hngFunc);
+moreBtn.addEventListener('click', moreFunc);
+migrantBtn.addEventListener('click', migrantFunc);
