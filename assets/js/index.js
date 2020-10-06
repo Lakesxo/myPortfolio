32be02413e943236af3a39d336b2a1c30a06026e
lakesxo <olaksajanaku@yahoo.com>
2020-10-06 10:56:54 +0100

    done with work experience section

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,21 +11,43 @@ menuToggle =()=> {
     menu.classList.toggle('change');
     navs.classList.toggle('hide');
 }
+
+// Tabbed content menu
 hngFunc =()=> {
     hng.style.display = "block";
     more.style.display = "none";
     migrant.style.display = "none"
+    hngBtn.style.color = "#480403"
+    hngBtn.style.borderLeft = "2px solid #480403"
+    moreBtn.style.color = "#C4C4C4"
+    moreBtn.style.borderLeft = "2px solid #C4C4C4"
+    migrantBtn.style.color = "#C4C4C4"
+    migrantBtn.style.borderLeft = "2px solid #C4C4C4"
 }
 migrantFunc =()=> {
     migrant.style.display = "block";
     more.style.display = "none";
     hng.style.display = "none";
+    migrantBtn.style.color = "#480403"
+    migrantBtn.style.borderLeft = "2px solid #480403"
+    hngBtn.style.color = "#C4C4C4"
+    hngBtn.style.borderLeft = "2px solid #C4C4C4"
+    moreBtn.style.color = "#C4C4C4"
+    moreBtn.style.borderLeft = "2px solid #C4C4C4"
 }
 moreFunc =()=> {
     more.style.display = "block";
     migrant.style.display = "none"
     hng.style.display = "none"
+    moreBtn.style.color = "#480403"
+    moreBtn.style.borderLeft = "2px solid #480403"
+    hngBtn.style.color = "#C4C4C4"
+    hngBtn.style.borderLeft = "2px solid #C4C4C4"
+    migrantBtn.style.color = "#C4C4C4"
+    migrantBtn.style.borderLeft = "2px solid #C4C4C4"
 }
+
+// Event listeners
 menu.addEventListener('click', menuToggle);
 hngBtn.addEventListener('click', hngFunc);
 moreBtn.addEventListener('click', moreFunc);
