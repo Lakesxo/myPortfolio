3a380de7c534aded72f88bf4b604c6737d9035c5
lakesxo <olaksajanaku@yahoo.com>
2020-10-08 14:18:08 +0100

    responsive experience section

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,6 +7,8 @@ let more = document.querySelector('.more');
 let hngBtn = document.querySelector('li.top');
 let moreBtn = document.querySelector('li.mid');
 let migrantBtn = document.querySelector('li.last');
+let mediaQuery = window.matchMedia("(max-width: 768px)")
+let xo = document.querySelector('ul.company li')
 
 // Hamburger menu function
 menuToggle =()=> {
@@ -20,33 +22,51 @@ hngFunc =()=> {
     more.style.display = "none";
     migrant.style.display = "none"
     hngBtn.style.color = "#480403"
-    hngBtn.style.borderLeft = "2px solid #480403"
+    if (mediaQuery.matches){
+        hngBtn.style.borderBottom = "2px solid #480403"
+        moreBtn.style.borderBottom = "2px solid #C4C4C4"
+        migrantBtn.style.borderBottom = "2px solid #C4C4C4"
+    } else {
+        hngBtn.style.borderLeft = "2px solid #480403"
+        moreBtn.style.borderLeft = "2px solid #C4C4C4"
+        migrantBtn.style.borderLeft = "2px solid #C4C4C4"
+    }
     moreBtn.style.color = "#C4C4C4"
-    moreBtn.style.borderLeft = "2px solid #C4C4C4"
     migrantBtn.style.color = "#C4C4C4"
-    migrantBtn.style.borderLeft = "2px solid #C4C4C4"
 }
 migrantFunc =()=> {
     migrant.style.display = "block";
     more.style.display = "none";
     hng.style.display = "none";
     migrantBtn.style.color = "#480403"
-    migrantBtn.style.borderLeft = "2px solid #480403"
+    if (mediaQuery.matches){
+        migrantBtn.style.borderBottom = "2px solid #480403"
+        hngBtn.style.borderBottom = "2px solid #C4C4C4"
+        moreBtn.style.borderBottom = "2px solid #C4C4C4"
+    } else {
+        migrantBtn.style.borderLeft = "2px solid #480403"
+        hngBtn.style.borderLeft = "2px solid #C4C4C4"
+        moreBtn.style.borderLeft = "2px solid #C4C4C4"
+    }
     hngBtn.style.color = "#C4C4C4"
-    hngBtn.style.borderLeft = "2px solid #C4C4C4"
     moreBtn.style.color = "#C4C4C4"
-    moreBtn.style.borderLeft = "2px solid #C4C4C4"
 }
 moreFunc =()=> {
     more.style.display = "block";
     migrant.style.display = "none"
     hng.style.display = "none"
     moreBtn.style.color = "#480403"
-    moreBtn.style.borderLeft = "2px solid #480403"
+    if (mediaQuery.matches){
+        moreBtn.style.borderBottom = "2px solid #480403"
+        hngBtn.style.borderBottom = "2px solid #C4C4C4"
+        migrantBtn.style.borderBottom = "2px solid #C4C4C4"
+    } else {
+        moreBtn.style.borderLeft = "2px solid #480403"
+        hngBtn.style.borderLeft = "2px solid #C4C4C4"
+        migrantBtn.style.borderLeft = "2px solid #C4C4C4"
+    }
     hngBtn.style.color = "#C4C4C4"
-    hngBtn.style.borderLeft = "2px solid #C4C4C4"
     migrantBtn.style.color = "#C4C4C4"
-    migrantBtn.style.borderLeft = "2px solid #C4C4C4"
 }
 
 // Event listeners
