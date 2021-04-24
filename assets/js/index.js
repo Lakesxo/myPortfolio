876cc1f8b6d7347b6b0b9b55deeefb9435c21fc5
unknown <lakes.ajanaku@gmail.com>
2021-04-24 15:32:30 +0100

    refactored code

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,7 +16,7 @@ let xo = document.querySelector('ul.company li');
 let viewMoreBtn = document.querySelector('button.viewMore');
 let moreExperience = document.querySelector('.thirdThree');
 let submitBtn = document.querySelector('.butSubmit');
-let mbNavLi = document.querySelectorAll('.mobile-nav li')
+let mbNavLi = document.querySelectorAll('.mobile-nav li');
 let header = document.querySelector('.head');
 const section = document.querySelector('.skew');
 let currentPos = window.pageYOffset;
@@ -36,7 +36,7 @@ const skew = () => {
 skew();
 
 // Hamburger menu function
-menuToggle =()=> {
+menuToggle = () => {
     menu.classList.toggle('change');
     navs.classList.toggle('hide');
     if (navs.classList.contains('nonezz')){
@@ -54,7 +54,7 @@ closeOnNavClick = () => {
 }
 
 // Close and open header on scroll
-window.addEventListener('scroll', function(e) {
+window.addEventListener('scroll', (e) => {
     last_scroll_position = window.scrollY;
   
     // Scrolling down
@@ -72,164 +72,130 @@ window.addEventListener('scroll', function(e) {
 });
 
 // Change orirntation of borders on screen change
-changeTabbedOnScreen =()=> {
+changeTabbedOnScreen = () => {
+    const allTabBtns = [moreBtn, hngBtn, migrantBtn, optisourceBtn, theBeesBtn];
     if (mediaQuery.matches){
-        moreBtn.style.borderBottom = "2px solid #C4C4C4"
-        moreBtn.style.borderLeft = '0px'
-        hngBtn.style.borderBottom = "2px solid #C4C4C4"
-        hngBtn.style.borderLeft = '0px'
-        migrantBtn.style.borderBottom = "2px solid #C4C4C4"
-        migrantBtn.style.borderLeft = '0px'
-        optisourceBtn.style.borderBottom = "2px solid #C4C4C4"
-        optisourceBtn.style.borderLeft = '0px'
-        theBeesBtn.style.borderBottom = "2px solid #C4C4C4"
-        theBeesBtn.style.borderLeft = '0px'
+        allTabBtns.forEach((tabBtn)=> {
+            tabBtn.classList.add('mdQueryStyling');
+            tabBtn.classList.remove('otherSizeStyling');
+            tabBtn.classList.remove('activeTabDesktop');
+        })
     } else {
-        moreBtn.style.borderLeft = "2px solid #C4C4C4"
-        moreBtn.style.borderBottom = '0px'
-        hngBtn.style.borderLeft = "2px solid #C4C4C4"
-        hngBtn.style.borderBottom = '0px'
-        migrantBtn.style.borderLeft = "2px solid #C4C4C4"
-        migrantBtn.style.borderBottom = '0px'
-        optisourceBtn.style.borderLeft = "2px solid #C4C4C4"
-        optisourceBtn.style.borderBottom = '0px'
-        theBeesBtn.style.borderLeft = "2px solid #C4C4C4"
-        theBeesBtn.style.borderBottom = '0px' 
+        allTabBtns.forEach((tabBtn)=> {
+            tabBtn.classList.add('otherSizeStyling');
+            tabBtn.classList.remove('activeTabMobile');
+            tabBtn.classList.remove('mdQueryStyling');
+        })
     }
 }
 
 // Function when Optisource tab is clicked
 optiTechFunc =()=> {
+    const optiArray = [moreBtn, hngBtn, migrantBtn, theBeesBtn]
     optisource.style.display = "block";
     hng.style.display = "none";
     migrant.style.display = "none";
     more.style.display = "none";
     beesNG.style.display = "none";
-    optisourceBtn.style.color = "#480403";
-    moreBtn.style.color = "#C4C4C4";
-    migrantBtn.style.color = "#C4C4C4";
-    hngBtn.style.color = "#C4C4C4";
-    theBeesBtn.style.color = "#C4C4C4";
+    optiArray.forEach((opt) => opt.style.color = "#C4C4C4")
     if (mediaQuery.matches){
-        optisourceBtn.style.borderBottom = "2px solid #480403"
-        hngBtn.style.borderBottom = "2px solid #C4C4C4"
-        moreBtn.style.borderBottom = "2px solid #C4C4C4"
-        migrantBtn.style.borderBottom = "2px solid #C4C4C4"
-        theBeesBtn.style.borderBottom = "2px solid #C4C4C4"
+        optisourceBtn.classList.add('activeTabMobile');
+        optisourceBtn.classList.remove('activeTabDesktop');
+        optiArray.forEach((l)=> l.classList.remove('activeTabMobile'));
+        optiArray.forEach((x)=> x.style.borderBottom = "2px solid #C4C4C4");
     } else {
-        optisourceBtn.style.borderLeft = "2px solid #480403"
-        hngBtn.style.borderLeft = "2px solid #C4C4C4"
-        moreBtn.style.borderLeft = "2px solid #C4C4C4"
-        migrantBtn.style.borderLeft = "2px solid #C4C4C4"
-        theBeesBtn.style.borderLeft = "2px solid #C4C4C4"
+        optisourceBtn.classList.add('activeTabDesktop');
+        optisourceBtn.classList.remove('activeTabMobile');
+        optiArray.forEach((zl)=> zl.classList.remove('activeTabDesktop'));
+        optiArray.forEach((xs)=> xs.style.borderLeft = "2px solid #C4C4C4");
     }
 }
 
 // Function when Hng tab is clicked
-hngFunc =()=> {
+hngFunc = () => {
+    const hngArray = [moreBtn, optisourceBtn, migrantBtn, theBeesBtn]
     hng.style.display = "block";
     more.style.display = "none";
     migrant.style.display = "none";
     optisource.style.display = "none";
     beesNG.style.display = "none";
-    hngBtn.style.color = "#480403";
-    moreBtn.style.color = "#C4C4C4";
-    migrantBtn.style.color = "#C4C4C4";
-    optisourceBtn.style.color = "#C4C4C4";
-    theBeesBtn.style.color = "#C4C4C4";
+    hngArray.forEach((hg) => hg.style.color = "#C4C4C4")
     if (mediaQuery.matches){
-        hngBtn.style.borderBottom = "2px solid #480403";
-        moreBtn.style.borderBottom = "2px solid #C4C4C4";
-        migrantBtn.style.borderBottom = "2px solid #C4C4C4";
-        optisourceBtn.style.borderBottom = "2px solid #C4C4C4";
-        theBeesBtn.style.borderBottom = "2px solid #C4C4C4";
+        hngBtn.classList.add('activeTabMobile');
+        hngBtn.classList.remove('activeTabDesktop');
+        hngArray.forEach((lh)=> lh.classList.remove('activeTabMobile'))
+        hngArray.forEach((hx)=> hx.style.borderBottom = "2px solid #C4C4C4")
     } else {
-        hngBtn.style.borderLeft = "2px solid #480403";
-        moreBtn.style.borderLeft = "2px solid #C4C4C4";
-        migrantBtn.style.borderLeft = "2px solid #C4C4C4";
-        optisourceBtn.style.borderLeft = "2px solid #C4C4C4";
-        theBeesBtn.style.borderLeft = "2px solid #C4C4C4";
+        hngBtn.classList.add('activeTabDesktop');
+        hngBtn.classList.remove('activeTabMobile');
+        hngArray.forEach((lhh)=> lhh.classList.remove('activeTabDesktop'));
+        hngArray.forEach((hxx)=> hxx.style.borderLeft = "2px solid #C4C4C4");
     }
 }
 
 // Function when Migrant Tab is clicked
-migrantFunc =()=> {
+migrantFunc = () => {
+    const migArray = [moreBtn, optisourceBtn, hngBtn, theBeesBtn]
     migrant.style.display = "block";
     more.style.display = "none";
     hng.style.display = "none";
     optisource.style.display = "none";
     beesNG.style.display = "none";
-    migrantBtn.style.color = "#480403";
-    hngBtn.style.color = "#C4C4C4";
-    moreBtn.style.color = "#C4C4C4";
-    optisourceBtn.style.color = "#C4C4C4";
-    theBeesBtn.style.color = "#C4C4C4";
+    migArray.forEach((mg) => mg.style.color = "#C4C4C4")
     if (mediaQuery.matches){
-        migrantBtn.style.borderBottom = "2px solid #480403";
-        hngBtn.style.borderBottom = "2px solid #C4C4C4";
-        moreBtn.style.borderBottom = "2px solid #C4C4C4";
-        optisourceBtn.style.borderBottom = "2px solid #C4C4C4";
-        theBeesBtn.style.borderBottom = "2px solid #C4C4C4";
+        migrantBtn.classList.add('activeTabMobile');
+        migrantBtn.classList.remove('activeTabDesktop');
+        migArray.forEach((mh)=> mh.classList.remove('activeTabMobile'))
+        migArray.forEach((mgx)=> mgx.style.borderBottom = "2px solid #C4C4C4")
     } else {
-        migrantBtn.style.borderLeft = "2px solid #480403";
-        hngBtn.style.borderLeft = "2px solid #C4C4C4";
-        moreBtn.style.borderLeft = "2px solid #C4C4C4";
-        optisourceBtn.style.borderLeft = "2px solid #C4C4C4";
-        theBeesBtn.style.borderLeft = "2px solid #C4C4C4";
+        migrantBtn.classList.add('activeTabDesktop');
+        migrantBtn.classList.remove('activeTabMobile');
+        migArray.forEach((mhh)=> mhh.classList.remove('activeTabDesktop'));
+        migArray.forEach((mgxx)=> mgxx.style.borderLeft = "2px solid #C4C4C4");
     }
 }
 
 // Function when More Tab is clicked
 moreFunc =()=> {
+    const moreArray = [migrantBtn, optisourceBtn, hngBtn, theBeesBtn]
     more.style.display = "block";
     migrant.style.display = "none";
     hng.style.display = "none";
     optisource.style.display = "none";
     beesNG.style.display = "none";
-    moreBtn.style.color = "#480403";
-    hngBtn.style.color = "#C4C4C4";
-    migrantBtn.style.color = "#C4C4C4";
-    optisourceBtn.style.color = "#C4C4C4";
-    theBeesBtn.style.color = "#C4C4C4";
+    moreArray.forEach((mg) => mg.style.color = "#C4C4C4")
     if (mediaQuery.matches){
-        moreBtn.style.borderBottom = "2px solid #480403";
-        hngBtn.style.borderBottom = "2px solid #C4C4C4";
-        migrantBtn.style.borderBottom = "2px solid #C4C4C4";
-        optisourceBtn.style.borderBottom = "2px solid #C4C4C4";
-        theBeesBtn.style.borderBottom = "2px solid #C4C4C4";
+        moreBtn.classList.add('activeTabMobile');
+        moreBtn.classList.remove('activeTabDesktop');
+        moreArray.forEach((mh)=> mh.classList.remove('activeTabMobile'))
+        moreArray.forEach((mgx)=> mgx.style.borderBottom = "2px solid #C4C4C4")
     } else {
-        moreBtn.style.borderLeft = "2px solid #480403";
-        hngBtn.style.borderLeft = "2px solid #C4C4C4";
-        migrantBtn.style.borderLeft = "2px solid #C4C4C4";
-        optisourceBtn.style.borderLeft = "2px solid #C4C4C4";
-        theBeesBtn.style.borderLeft = "2px solid #C4C4C4";
+        moreBtn.classList.add('activeTabDesktop');
+        moreBtn.classList.remove('activeTabMobile');
+        moreArray.forEach((mhh)=> mhh.classList.remove('activeTabDesktop'));
+        moreArray.forEach((mgxx)=> mgxx.style.borderLeft = "2px solid #C4C4C4");
     }
 }
 
 // Function when TheBees tab is clicked
 theBeesFunc =()=> {
+    const beesArray = [migrantBtn, optisourceBtn, hngBtn, moreBtn]
     beesNG.style.display = "block";
     optisource.style.display = "none";
     hng.style.display = "none";
     migrant.style.display = "none";
     more.style.display = "none";
-    theBeesBtn.style.color = "#480403";
-    moreBtn.style.color = "#C4C4C4";
-    migrantBtn.style.color = "#C4C4C4";
-    hngBtn.style.color = "#C4C4C4";
-    optisourceBtn.style.color = "#C4C4C4";
+    beesArray.forEach((mg) => mg.style.color = "#C4C4C4")
     if (mediaQuery.matches){
-        theBeesBtn.style.borderBottom = "2px solid #480403"
-        hngBtn.style.borderBottom = "2px solid #C4C4C4"
-        moreBtn.style.borderBottom = "2px solid #C4C4C4"
-        migrantBtn.style.borderBottom = "2px solid #C4C4C4"
-        optisourceBtn.style.borderBottom = "2px solid #C4C4C4"
+        theBeesBtn.classList.add('activeTabMobile');
+        theBeesBtn.classList.remove('activeTabDesktop');
+        beesArray.forEach((mh)=> mh.classList.remove('activeTabMobile'));
+        beesArray.forEach((mgx)=> mgx.style.borderBottom = "2px solid #C4C4C4");
     } else {
-        theBeesBtn.style.borderLeft = "2px solid #480403"
-        hngBtn.style.borderLeft = "2px solid #C4C4C4"
-        moreBtn.style.borderLeft = "2px solid #C4C4C4"
-        migrantBtn.style.borderLeft = "2px solid #C4C4C4"
-        optisourceBtn.style.borderLeft = "2px solid #C4C4C4"
+        theBeesBtn.classList.add('activeTabDesktop');
+        theBeesBtn.classList.remove('activeTabMobile');
+        beesArray.forEach((mhh)=> mhh.classList.remove('activeTabDesktop'));
+        beesArray.forEach((mgxx)=> mgxx.style.borderLeft = "2px solid #C4C4C4");
     }
 }
 
