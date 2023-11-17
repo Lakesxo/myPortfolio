246fd06046d3f940360a63b4761863fea7594590
Lakes <lakes.ajanaku@gmail.com>
2023-11-17 12:44:26 +0100

    fix: (#8) Adjusted 4k screen width

--- new-portfolio/src/components/header/Header.tsx
+++ new-portfolio/src/components/header/Header.tsx
@@ -7,32 +7,34 @@ interface HeaderProps {}
 const Header: React.FunctionComponent<HeaderProps> = () => {
   const navigate = useNavigate();
   return (
-    <div className="header">
-      <a onClick={() => navigate("/")}>
-        <div className="logo">
-          <img
-            src="https://res.cloudinary.com/dt9pwfpi5/image/upload/v1693737575/Ellipse_13_mregnx.png"
-            alt="ridwan ajanaku"
-            draggable={false}
-          />
+    <div className="headerWrapper">
+      <div className="header">
+        <a onClick={() => navigate("/")}>
+          <div className="logo">
+            <img
+              src="https://res.cloudinary.com/dt9pwfpi5/image/upload/v1693737575/Ellipse_13_mregnx.png"
+              alt="ridwan ajanaku"
+              draggable={false}
+            />
+          </div>
+        </a>
+        <div className="header-links">
+          <a onClick={() => navigate("/")} className="links">
+            Home
+          </a>
+          <a onClick={() => navigate("/about")} className="links">
+            About me
+          </a>
+          <a onClick={() => navigate("/works")} className="links">
+            Works
+          </a>
+          <a onClick={() => navigate("/contact")} className="links">
+            Contact me
+          </a>
+          <button className="theme">
+            <MoonIcon />
+          </button>
         </div>
-      </a>
-      <div className="header-links">
-        <a onClick={() => navigate("/")} className="links">
-          Home
-        </a>
-        <a onClick={() => navigate("/about")} className="links">
-          About me
-        </a>
-        <a onClick={() => navigate("/works")} className="links">
-          Works
-        </a>
-        <a onClick={() => navigate("/contact")} className="links">
-          Contact me
-        </a>
-        <button className="theme">
-          <MoonIcon />
-        </button>
       </div>
     </div>
   );
